56153e6eb08a4e72b98704202025efaf6f87b441
Mohammed Minhaj <nijulinux@gmail.com>
2024-12-03 17:15:42 +0530

    footer added

--- src/app/features/main/pages/homepage/homepage.component.ts
+++ src/app/features/main/pages/homepage/homepage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/partials/navbar/navbar.component';
 import { OlxautosBannerComponent } from '../../../../shared/partials/olxautos-banner/olxautos-banner.component';
 import { FreshRecommendationsComponent } from './fresh-recommendations/fresh-recommendations.component';
+import { FooterComponent } from '../../../../shared/partials/footer/footer.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavbarComponent, OlxautosBannerComponent, FreshRecommendationsComponent],
+  imports: [NavbarComponent, OlxautosBannerComponent, FreshRecommendationsComponent, FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
